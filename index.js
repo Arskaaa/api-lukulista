6c58b2658c95bff5e7e66f35d46fb755231838e3
Arskaaa <90454026+Arskaaa@users.noreply.github.com>
2021-10-08 14:40:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+function arvoNumero(alaraja, yläraja) {
+      return Marh.floor(Math random()) + alaraja
+}
+
+
 app.get('/', function (req, res) {
-  res.send('1 5 8 2 5')
+  const NUMEROLISTA = [
+      arvoNumero(0,5),
+      arvoNumero(10,15),
+      arvoNumero(100,500),
+      arvoNumero(10,20),
+      arvoNumero(0,5)
+  ]
+  res.json(numerolista)
   })
   
   app.listen(PORT)
